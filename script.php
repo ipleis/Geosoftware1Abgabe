3e42be7b898d8b375125d05807b6db5eeb9fac9b
Ilka Pleiser <ilpl@esri-de.com>
2018-08-14 15:32:41 +0200

    #new ajax

---
+++ script.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>fachbereiche</titel>
+  </head>
+  <body>
+    <?php
+      echo 'Facbereich: ',$_POST['input1'],'Abkürzung: ',$_POST['input2'],'Internetadresse: ',$_POST['input3'], 'zugehörige Institute: ',$_POST['input4', 'Adresse: ',$_POST['input5'];
+    ?>
