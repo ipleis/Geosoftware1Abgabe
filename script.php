f4ad586e491f68da0b495929b60c5d2af03b88f0
ipleis <38665063+ipleis@users.noreply.github.com>
2018-08-15 16:24:55 +0200

    #finish Adressen

--- script.php
+++ script.php
@@ -7,3 +7,5 @@
     <?php
       echo 'Facbereich: ',$_POST['input1'],'Abkürzung: ',$_POST['input2'],'Internetadresse: ',$_POST['input3'], 'zugehörige Institute: ',$_POST['input4', 'Adresse: ',$_POST['input5'];
     ?>
+  </body>
+</html>
