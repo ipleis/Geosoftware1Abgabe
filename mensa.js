f7db7f578ae5c414c8209aebd9b08930750c9f70
ipleis <ilka.pleiser@web.de>
2018-08-19 15:32:04 +0200

    #new jade

--- mensa.js
+++ mensa.js
@@ -19,12 +19,107 @@ btn.addEventListener("click", function(){
 
 function renderHTML(data){
   var htmlString = "";
+  var i;
 
-  for (i=0; i < data.length; i++){
-    htmlString += "<p>" + data[0].date + " : " + data[0].meals.name + "</p>";
-    for(ii=0; ii < data[i].meals.length; ii++){
-      htmlString += "<p>" + data[i].meals.prices + "</p>";
-    }
+    htmlString += "<p>" + data[0].date + ": " + "</p>";
+
+    for(i=0; i<data[0].meals.length; i++) {
+      htmlString += "<p>" + data[0].meals[i].category + ": " + data[0].meals[i].name  + "</p>";
+      htmlString += "<p>" + "Preis (Studenten): " + data[0].meals[i].prices.students + "€" + "</p>";
+      htmlString += "<p>" + "Preis (Mitarbeiter): " + data[0].meals[i].prices.employees  + "€" + "</p>";
+      htmlString += "<p>" + "Preis (Andere): " + data[0].meals[i].prices.others  + "€" + "</p>";
+
+}
+
+  mensaContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+
+
+//Mensa am Ring
+btn1.addEventListener("click", function(){
+  var myRequest = new XMLHttpRequest();
+  myRequest.open('GET', 'http://openmensa.org/api/v2/canteens/225/meals');
+  myRequest.onload = function() {
+    var myData = JSON.parse(myRequest.responseText);
+    renderHTML(myData);
+  };
+  myRequest.send();
+
+});
+
+function renderHTML(data){
+  var htmlString = "";
+  var i;
+
+    htmlString += "<p>" + data[0].date + ": " + "</p>";
+
+    for(i=0; i<data[0].meals.length; i++) {
+      htmlString += "<p>" + data[0].meals[i].category + ": " + data[0].meals[i].name  + "</p>";
+      htmlString += "<p>" + "Preis (Studenten): " + data[0].meals[i].prices.students + "€" + "</p>";
+      htmlString += "<p>" + "Preis (Mitarbeiter): " + data[0].meals[i].prices.employees  + "€" + "</p>";
+      htmlString += "<p>" + "Preis (Andere): " + data[0].meals[i].prices.others  + "€" + "</p>";
+
+}
+
+  mensaContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+
+
+
+//Mensa Da Vinci
+btn2.addEventListener("click", function(){
+  var myRequest = new XMLHttpRequest();
+  myRequest.open('GET', 'http://openmensa.org/api/v2/canteens/228/meals');
+  myRequest.onload = function() {
+    var myData = JSON.parse(myRequest.responseText);
+    renderHTML(myData);
+  };
+  myRequest.send();
+
+});
+
+function renderHTML(data){
+  var htmlString = "";
+  var i;
+
+    htmlString += "<p>" + data[0].date + ": " + "</p>";
+
+    for(i=0; i<data[0].meals.length; i++) {
+      htmlString += "<p>" + data[0].meals[i].category + ": " + data[0].meals[i].name  + "</p>";
+      htmlString += "<p>" + "Preis (Studenten): " + data[0].meals[i].prices.students + "€" + "</p>";
+      htmlString += "<p>" + "Preis (Mitarbeiter): " + data[0].meals[i].prices.employees  + "€" + "</p>";
+      htmlString += "<p>" + "Preis (Andere): " + data[0].meals[i].prices.others  + "€" + "</p>";
+
+}
+
+  mensaContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+
+
+
+//Mensa Bispinghof
+btn3.addEventListener("click", function(){
+  var myRequest = new XMLHttpRequest();
+  myRequest.open('GET', 'http://openmensa.org/api/v2/canteens/233/meals');
+  myRequest.onload = function() {
+    var myData = JSON.parse(myRequest.responseText);
+    renderHTML(myData);
+  };
+  myRequest.send();
+
+});
+
+function renderHTML(data){
+  var htmlString = "";
+  var i;
+
+    htmlString += "<p>" + data[0].date + ": " + "</p>";
+
+    for(i=0; i<data[0].meals.length; i++) {
+      htmlString += "<p>" + data[0].meals[i].category + ": " + data[0].meals[i].name  + "</p>";
+      htmlString += "<p>" + "Preis (Studenten): " + data[0].meals[i].prices.students + "€" + "</p>";
+      htmlString += "<p>" + "Preis (Mitarbeiter): " + data[0].meals[i].prices.employees  + "€" + "</p>";
+      htmlString += "<p>" + "Preis (Andere): " + data[0].meals[i].prices.others  + "€" + "</p>";
 
 }
 
